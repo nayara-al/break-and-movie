d676fb2a3b0bc5a6f0bc4b8afc87da3933281737
nayara-al <anna_nayara@hotmail.com>
2024-03-21 21:43:24 -0300

    refactor: ajuste de estilização

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,7 +3,7 @@ import BoxBase from "./BaseBox";
 import Button from "./Button";
 import Footer from "./layout/Footer";
 import StructurePage from "./layout/StructurePage";
-import { Movie } from "./Movie";
+import { MovieCard } from "./MovieComponents";
 import { FieldText } from "./FieldText";
 
-export { StructurePage, Header, Button, BoxBase, FieldText, Footer, Movie };
+export { StructurePage, Header, Button, BoxBase, FieldText, Footer, MovieCard as Movie };
