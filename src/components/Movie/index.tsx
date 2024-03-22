c719061f4048e9d0bafdcf5db2a4c1ff12aaa1e4
nayara-al <anna_nayara@hotmail.com>
2024-03-21 21:42:47 -0300

    refactor: renomeando pastas e arquivos

--- src/components/Movie/index.tsx
+++
@@ -1,4 +0,0 @@
-import Image from "./Image";
-import MovieBox from "./MovieBox";
-
-export const Movie = { Image, MovieBox };
---
+++ src/components/MovieComponents/index.tsx
@@ -0,0 +1,5 @@
+import Image from "./Image";
+import MovieBox from "./MovieBox";
+import LinkButton from "./LinkButton";
+
+export const MovieCard = { Image, MovieBox, LinkButton };
