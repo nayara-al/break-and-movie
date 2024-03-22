1777f806097ebfac99d03930a33645d93f36ca3b
nayara-al <anna_nayara@hotmail.com>
2024-03-21 21:42:18 -0300

    refactor: Ajuste de componentes de layout

--- src/components/layout/StructurePage/index.tsx
+++ src/components/layout/StructurePage/index.tsx
@@ -3,7 +3,7 @@ import { HTMLAttributes } from "react";
 interface StructurePageProps extends HTMLAttributes<HTMLDivElement> {}
 export default function StructurePage({ ...props }: StructurePageProps) {
   return (
-    <main {...props} className="p-10 flex flex-col items-center justify-center">
+    <main {...props} className="overflow-hidden py-10 flex flex-col items-center justify-center">
       {props.children}
     </main>
   );
