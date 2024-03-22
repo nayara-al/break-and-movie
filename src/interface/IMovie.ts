e53f49659b8e29df816b84ad96377e05c89f4c87
nayara-al <anna_nayara@hotmail.com>
2024-03-21 21:42:58 -0300

    refactor: ajuste de interface

--- src/interface/IMovie.ts
+++ src/interface/IMovie.ts
@@ -2,4 +2,9 @@ export interface IMovie {
   poster_path: string;
   original_title?: string;
   original_name?: string;
+  id: string;
+  budget?: number;
+  revenue?: number;
+  runtime?: string;
+  overview?: string;
 }
